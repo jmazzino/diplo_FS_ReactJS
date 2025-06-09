b15df190f6e83ed9f65958f47e43a436a179a6bc
jmazzino <jorge.mazzino@gmail.com>
2025-06-08 21:11:13 -0300

    Subiendo ejercicio

---
+++ m4u1/mi_ejemplo/front/src/app/nosotros/page.js
@@ -0,0 +1,51 @@
+
+export default function Nosotros() {
+    return (
+        <div >
+            <main className="holder">
+                <div className="historia">
+                    <h2>Historia</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+
+                </div>
+                <div className="staff">
+                    <h2>Staff</h2>
+                    <div className="personas">
+                        <div className="persona">
+                            <img src="images/nosotros/nosotros1.jpg" alt="Juan Gomez"></img>
+                            <h5>Juan Gomez</h5>
+                            <h6>Gerente General</h6>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                        </div>
+                        <div className="persona">
+                            <img src="images/nosotros/nosotros2.jpg" alt="Diana Reyes"></img>
+                            <h5>Diana Reyes</h5>
+                            <h6>Gerente Comercial</h6>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                        </div>
+                        <div className="persona">
+                            <img src="images/nosotros/nosotros3.jpg" alt="Roberto Zapatos"></img>
+                            <h5>Roberto Zapatos</h5>
+                            <h6>Gerente de Sistemas</h6>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                        </div>
+                        <div className="persona">
+                            <img src="images/nosotros/nosotros4.jpg" alt="Sandra Mastropiero"></img>
+                            <h5>Sandra Mastropiero</h5>
+                            <h6>Gerente de Marketing</h6>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                        </div>
+                        <div className="persona">
+                            <img src="images/nosotros/nosotros5.jpg" alt="Luciano Figuero"></img>
+                            <h5>Luciano Figuero</h5>
+                            <h6>Gerente Logistica</h6>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                        </div>
+                    </div>
+                </div>
+            </main>
+        </div>
+
+    );
+}
---
+++ m4u1/mi_ejemplo/front/src/app/page.js
@@ -0,0 +1,29 @@
+
+export default function Home() {
+  return (
+    <div >
+      <main className="holder">
+        <div>
+          <img src="images//home/img01.jpg" alt="avion"></img>
+        </div>
+
+        <div className="columnas">
+          <div className="bienvenidos">
+            <h2>Bienvenidos</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+          </div>
+
+          <div className="testimonios">
+            <h2>Testimonios</h2>
+            <div className="testimonio">
+              <span className="cita">Simplemente Excelente</span>
+              <span className="autor">Juan Perez - zapatos.com</span>
+            </div>
+          </div>
+        </div>
+      </main>
+
+    </div>
+  );
+}
