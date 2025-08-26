2e151c7bf37b737f7da7bbb9e44556b948fc26e5
jmazzino <jorge.mazzino@gmail.com>
2025-08-26 08:05:27 -0300

    test

--- m4u4/mi_ejemplo/back/app.js
+++ m4u4/mi_ejemplo/back/app.js
@@ -7,6 +7,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+var loginRouter = require('./routes/admin/login');
+
 var app = express();
 
 // view engine setup
