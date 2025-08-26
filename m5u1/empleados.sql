f64302cb218c620af15881ce119ab2b15240e246
jmazzino <jorge.mazzino@gmail.com>
2025-08-26 16:28:22 -0300

    m5u1

---
+++ m5u1/empleados.sql
@@ -0,0 +1,77 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1:3306
+-- Tiempo de generación: 26-08-2025 a las 19:24:05
+-- Versión del servidor: 9.1.0
+-- Versión de PHP: 8.3.14
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `ejercicio`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `empleados`
+--
+
+DROP TABLE IF EXISTS `empleados`;
+CREATE TABLE IF NOT EXISTS `empleados` (
+  `id_emp` int NOT NULL AUTO_INCREMENT,
+  `nombre` text NOT NULL,
+  `apellido` text NOT NULL,
+  `trabajo` text NOT NULL,
+  `edad` int NOT NULL,
+  `salario` int NOT NULL,
+  `mail` text NOT NULL,
+  PRIMARY KEY (`id_emp`)
+) ENGINE=MyISAM AUTO_INCREMENT=28 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Volcado de datos para la tabla `empleados`
+--
+
+INSERT INTO `empleados` (`id_emp`, `nombre`, `apellido`, `trabajo`, `edad`, `salario`, `mail`) VALUES
+(1, 'Juan', 'Hagan', 'Programador Senior\r\n', 32, 120000, '_juanhagan@bignet.com\r\n'),
+(2, 'Gonzalo', 'Pillai', 'Programador Senior', 32, 110000, 'g_pillai@bignet.com\r\n'),
+(3, 'Ana', 'Dharma', 'Desarrollador Web\r\n', 27, 90000, 'ana@bignet.com\r\n'),
+(4, 'Maria', 'Anchor', 'Desarrollador Web\r\n', 26, 85000, 'mary@bignet.com\r\n'),
+(5, 'Juan', 'Agüero', 'Programador', 36, 85000, 'juan@bignet.com'),
+(6, 'Eduardo', 'Sacan', 'Programador', 25, 85000, 'eddi@bignet.com'),
+(7, 'Alejandro', 'Nanda', 'Programador', 32, 70000, 'alenanda@bignet.com'),
+(8, 'Juan', 'Agüero', 'Programador', 36, 85000, 'juan@bignet.com'),
+(10, 'Alejandro', 'Nanda', 'Programador', 32, 70000, 'alenanda@bignet.com'),
+(11, 'Eduardo', 'Sacan', 'Programador', 25, 85000, 'eddi@bignet.com'),
+(12, 'Alejandro', 'Nanda', 'Programador', 32, 70000, 'alenanda@bignet.com'),
+(13, 'Eduardo', 'Sacan', 'Programador', 25, 85000, 'eddi@bignet.com'),
+(14, 'Alejandro', 'Nanda', 'Programador', 32, 70000, 'alenanda@bignet.com'),
+(15, 'Hernan', 'Rosso', 'Especialista Multimedia', 33, 90000, 'hernan@bignet.com'),
+(16, 'Pablo', 'Simon', 'Especialista Multimedia', 43, 85000, 'ps@bignet.com'),
+(17, 'Arturo', 'Hernandez', 'Especialista Multimedia', 32, 75000, 'arturo@bignet.com'),
+(18, 'Jimena', 'Cazado', 'Diseñador Web', 32, 110000, 'jimena@bignet.com'),
+(19, 'Roberto', 'Luis', 'Administrador de sistemas', 35, 100000, 'roberto@bignet.com'),
+(20, 'Daniel', 'Gutierrez', 'Administrador de sistemas', 34, 900000, 'daniel@bignet.com'),
+(21, 'Miguel', 'Harper', 'Ejecutivo de Ventas Senior', 36, 120000, ' miguel@bignet.com'),
+(22, 'Monica', 'Sanchez', 'Ejecutivo de ventas', 30, 90000, 'monica@bignet.com'),
+(23, 'Alicia', 'Simlai', 'Ejecutivo de ventas', 27, 70000, 'alicia@bignet.com'),
+(24, 'Jose', 'Iriarte', 'Ejecutivo de ventas', 27, 72000, 'jose@bignet.com'),
+(25, 'Sabrina', 'Allende', 'Gerente de Soporte tecnico', 32, 200000, 'sabrina@bignet.com'),
+(26, 'Pedro', 'Campeon', 'Gerente de finanzas', 36, 220000, 'pedro@bignet.com'),
+(27, 'Mariano', 'Dharma', 'Presidente', 28, 300000, 'mariano@bignet.com');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
